80e9ab57b24cc10e239c82ad049a7171fd60dc90
Chelsea Sept. Thiani Puri <43957609+chelseastp@users.noreply.github.com>
2020-03-03 21:45:01 +0700

    update two

---
+++ novice/01-02/latihan/example3.js
@@ -0,0 +1,17 @@
+switch (fruittype){
+    case 'Oranges':
+        console.log()('Oranges are $0.59 a pound.');
+        break;
+    case 'Apples':
+        console.log()('Apples are $0.32 a pound.');
+        break;
+    case 'Bananas':
+        console.log()('Bananas are $0.48 a pound.');
+        break;
+    case 'Cherries':
+        console.log('Mangoes are $0.56 a pound');
+        break;
+    case 'Papayas':
+        console.log()
+
+    }
