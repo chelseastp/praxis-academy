0d7edbf4e6aa48ee9b6c446f29243cc9b69b5b3d
Chelsea Sept. Thiani Puri <43957609+chelseastp@users.noreply.github.com>
2020-04-01 09:53:19 +0700

    baru up

---
+++ novice/03-01/kasus/retry.js
@@ -0,0 +1,19 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+    if (Math.random() < 0.2) {
+        return a*b;
+    } else {
+        throw new MultiplicatorUnitFailure("Klunk");
+    }
+}
+
+function reliableMultiply(a, b) {
+    if (Math.random() < 0.2) {
+        return a*b;
+    } else {
+        throw new MultiplicatorUnitFailure("Klunk");
+    }
+}
+
+console.log(reliableMultiply(8, 8));
